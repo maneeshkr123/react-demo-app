5ef08c41007f88b7cae29d64a637bf76f4cf9130
Maneesh Kumar <maneesh.kumar@staqo.com>
2024-01-05 20:59:52 +0530

    Maneesh :: Initial Commit

---
+++ src/pages/Menu.jsx
@@ -0,0 +1,11 @@
+// @flow 
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+
+export const Menu = () => {
+    return (
+        <>
+            <Link to={'/hello'}>Hello</Link>
+        </>
+    );
+};  
