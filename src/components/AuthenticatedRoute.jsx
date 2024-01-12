72107e8d1821970c589bc21ce29d6dba8b184f25
Maneesh Kumar <maneeshkr.123@gmail.com>
2024-01-12 22:37:12 +0530

    Maneesh :: new Material UI header & sidebar added

--- src/components/AuthenticatedRoute.jsx
+++ src/components/AuthenticatedRoute.jsx
@@ -3,15 +3,12 @@ import * as React from 'react';
 import { Navigate } from 'react-router-dom';
 import { checkLogin } from '../controllers/AuthController';
 import Header from './layout/Header';
-import Footer from './layout/Footer';
 
 export function AuthenticatedRoute({ children }) {
     return checkLogin() ?
         (
             <>
-                <Header />
-                {children}
-                <Footer />
+                <Header>{children}</Header>
             </>
         )
         :
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,48 @@
+import { Grid } from '@mui/material'
 import React from 'react'
 
 const HomePage = () => {
+  const blogs = [
+    {
+      "title": "Blog 1",
+      "desc": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur, ad?"
+    },
+    {
+      "title": "Blog 2",
+      "desc": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur, adLorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur, adLorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur, ad ?"
+    },
+    {
+      "title": "Blog 3",
+      "desc": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur, ad?"
+    },
+    {
+      "title": "Blog 4",
+      "desc": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur, ad?"
+    },
+  ]
   return (
-    <div>HomePage</div>
+    <>
+
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        {blogs.map((item, index) => {
+          return (
+            <>
+              <Grid item xs={2} sm={4} md={4} key={index}>
+                <h4>
+                  {item.title}
+                  </h4>
+                <p>
+                  {item.desc}
+                  </p>
+              </Grid>
+            </>
+
+          )
+        })}
+
+      </Grid>
+
+    </>
   )
 }
 
